100fef526a49e1ea1283fc99d5aa7945177acf46
Swaraj <panigrahiswaraj89@gmail.com>
2021-11-24 07:59:47 +0530

    Slides updated. Css issues fixed.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <SlidesList />
+      <p className="warn"> <sup>*</sup> Best viewed at 976px and below</p>
     </div>
   );
 }
--- src/config/SlidesData.config.js
+++ src/config/SlidesData.config.js
@@ -4,19 +4,103 @@ const Slides = {
         innerHtml: 'Welcome, Team!'
     }, {
         title: 'what',
-        innerHtml: 'Nifty features of <br /> <span class="highlight"> chrome dev tool! </span>'
+        innerHtml: 'Nifty features of <br /> <p class="highlight"> chrome dev tool! 🚀🚀 </p>'
     }, {
         title: '1',
-        innerHtml: '1. Command Menu <code>cmd + shift + p</code> '
+        innerHtml: 'Command menu is your saviour 🌟 <br> <code>cmd + shift + p</code> '
     }, {
         title: '2',
-        innerHtml: '2. Rearranging tabs '
+        innerHtml: `
+            <p>
+                Shortcuts
+            </p>
+            <ul>
+                <li> <code>cmd + option + i</code><br />Open dev tool with last tab opened<br /></li>
+                <li> <code>cmd + option + j</code><br />Open dev tool with console<br /></li>
+                <li> <code>cmd + shift + c</code><br />Inspect element<br /></li>
+                <li> <code>cmd + shift + m</code><br />Toggle Mobile view<br /></li>
+                <li> <code>cmd + shipt + p</code><br />Open command menu<br /></li>
+            </ul>
+        `
     }, {
         title: '3',
-        innerHtml: '3. Node drag & drop  '
+        innerHtml: 'Node drag & drop'
     }, {
         title: '4',
-        innerHtml: '4. CSS values slide'
+        innerHtml: 'Color Picker <code>Accesibility</code>'
+    }, {
+        title: '5',
+        innerHtml: 'Accesibility tab'
+    }, {
+        title: '6',
+        innerHtml: '<div>CSS values slide <ul><li><code>cmd + up/down arrow</code></li><li><code>option + up/down arrow</code></li><li><code>Mouse Slider</code></li></ul></div>'
+    }, {
+        title: '7',
+        innerHtml: 'Auto-complete CSS'
+    }, {
+        title: '8',
+        innerHtml: 'Network tab <br /><code>Disable cache</code>'
+    }, {
+        title: '9',
+        innerHtml: 'Network tab <br /><code>Throttling & screen capture</code>'
+    }, {
+        title: '10',
+        innerHtml: 'Capture page load screenshots '
+    }, {
+        title: '11',
+        innerHtml: 'Multi filter select <br /><code>cmd + click</code>'
+    }, {
+        title: '12',
+        innerHtml: `
+        <p>
+            Network tab Filter Search
+        </p>
+        <ul>
+            <li> <code>larger than:</code></li>
+            <li> <code>Regex:</code></li>
+            <li> <code>domain:</code></li>
+        </ul>
+        `
+    }, {
+        title: '13',
+        innerHtml: `
+        <p>
+            Network Tab <br/><code>Sorting</code>
+        </p>`
+    }, {
+        title: '14',
+        innerHtml: `Network tab <br/> <code>'Copy as'</code>`
+    }, {
+        title: '15',
+        innerHtml: `Full view screenshots 📸`
+    }, {
+        title: '16',
+        innerHtml: `Node Screenshots 📸`
+    }, {
+        title: '17',
+        innerHtml: `Lighthouse 🚨`
+    }, {
+        title: '18',
+        innerHtml: `
+        <p>
+            Refrences/Further Reading
+        </p>
+        <ul>
+            <li> <a href="https://developer.chrome.com/docs/devtools/" class="highlight">Official Documentation</a></li>
+            <li> <a href="https://umaar.com/dev-tips/" class="highlight">Umar Dev Tips</a></li>
+            <li> <a href="https://kayce.basqu.es/" class="highlight">Kayce Basques</a></li>
+        </ul>
+        `
+    }, {
+        title: '19',
+        innerHtml: `
+        <p>
+            Help me improve
+        </p>
+            <a href="https://bit.ly/3HPqIW4" class="highlight">https://bit.ly/3HPqIW4</a>
+            <br />
+            <img src="../feedback.png" alt="HTML5 Doctor Logo" />
+        `
     }]
 };
 
